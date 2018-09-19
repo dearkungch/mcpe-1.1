6ee728d0d604bb86a541e415d63a708d0a9ba500
Dearkung <ndear2012@gmail.com>
2018-09-19 22:40:25 +0700

    fix start.sh

--- start.sh
+++ start.sh
@@ -36,9 +36,9 @@ while getopts "p:f:l" OPTION 2> /dev/null; do
 done
 
 if [ "$PHP_BINARY" == "" ]; then
-	if [ -f ./bin/php7/bin/php ]; then
+	if [ -f ./bin/php ]; then
 		export PHPRC=""
-		PHP_BINARY="./bin/php7/bin/php"
+		PHP_BINARY="./bin/php"
 	elif type php 2>/dev/null; then
 		PHP_BINARY=$(type -p php)
 	else
